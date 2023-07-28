8bd652f592aaadcd47589e7f70f045c2582fcc53
TecGabriel-Silva <Tec.gabriel2003@gmail.com>
2023-07-28 20:15:39 -0300

    primeiro commit

--- app/controllers/register_products_controller.rb
+++ app/controllers/register_products_controller.rb
@@ -56,7 +56,7 @@ class RegisterProductsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_register_product
