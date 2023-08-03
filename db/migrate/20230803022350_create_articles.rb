16fe44e17f98a7aaa203db8032a893d6530ce475
TecGabriel-Silva <Tec.gabriel2003@gmail.com>
2023-08-03 00:22:38 -0300

    dia 03/08

---
+++ db/migrate/20230803022350_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_022350) do
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
