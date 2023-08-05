79aa48ff71d60b6f4fa2d2e93fad97eb538bb543
TecGabriel-Silva <Tec.gabriel2003@gmail.com>
2023-08-04 23:33:34 -0300

    Update_Product

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,42 @@ class ArticlesController < ApplicationController
   def index
     @article = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def create 
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  #Uma funcao privada não pode ser acessada pelas views
+  private 
+  def article_params
+    params.require(:article).permit(:name, :description, :price)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    #validar dados que serao inseridos no new
+    validates :name, presence: true #Torncando campos obrigatórios
+    validates :description, presence: true 
+    validates :price, presence: true, length: {minimum: 1} #informando que o valor minimo é 1 
 end
